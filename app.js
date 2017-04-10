a228123acebccf825ff539d66f3164b597b259d5
James Attard <info@jamesattard.com>
2017-04-10 11:26:12 +0200

    implemented routes for comments

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ express               = require("express"),
 app                   = express();
 
 // Models
-var Campground  = require("./models/campground"),
-Comment         = require("./models/comment"),
-User            = require("./models/user");
+var Campground        = require("./models/campground"),
+Comment               = require("./models/comment"),
+User                  = require("./models/user");
 
 // App Config
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 
@@ -27,13 +27,17 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+// ===================
+// Campgrounds Routes
+// ===================
+
 // INDEX Route
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, allCampgrounds){
-    if (err){
+    if (err) {
       console.log(err);
     } else {
-      res.render("index",
+      res.render("campgrounds/index",
         {campgrounds:allCampgrounds}
       );
     }
@@ -42,7 +46,7 @@ app.get("/campgrounds", function(req, res){
 
 // NEW Route
 app.get("/campgrounds/new", function(req, res){
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 // CREATE Route
@@ -57,7 +61,7 @@ app.post("/campgrounds", function(req, res){
   };
 
   Campground.create(newCampground, function(err, newlyCreated){
-    if (err){
+    if (err) {
       console.log(err);
     } else {
       res.redirect("/campgrounds");
@@ -68,11 +72,11 @@ app.post("/campgrounds", function(req, res){
 // SHOW Route
 app.get("/campgrounds/:id", function(req, res){
   Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground){
-    if (err){
+    if (err) {
       console.log(err);
     } else {
       console.log(foundCampground);
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
 });
@@ -82,6 +86,41 @@ app.get("/campgrounds/:id", function(req, res){
 // UPDATE Route
 
 // DESTROY Route
+
+// ================
+// Comments Routes
+// ================
+
+// NEW Route
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  })
+});
+
+// CREATE Route
+app.post("/campgrounds/:id/comments", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+      redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("YelpCamp server has started...");
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
   {
     name: "Silver Crest",
     image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg",
-    description: "bla bla bla"
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
   },
   {
     name: "Blue Rocks",
     image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
-    description: "bla bla bla"
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
   },
   {
     name: "Orange Sunset",
     image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg",
-    description: "bla bla bla"
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
   }
 ];
 
