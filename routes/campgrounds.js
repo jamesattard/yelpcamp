f8dbdc4eb705ab024946ca1e0b7e399c8375a7dc
James Attard <info@jamesattard.com>
2017-05-01 07:34:23 +0200

    v9

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,10 +29,15 @@ router.post("/", isLoggedIn, function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var description = req.body.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
   var newCampground = {
     name: name,
     image: image,
-    description: description
+    description: description,
+    author: author
   };
 
   Campground.create(newCampground, function(err, newlyCreated){
