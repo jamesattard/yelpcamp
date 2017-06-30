7b5408105e479a849fb5d5d9acccfbc7e5f48c0a
James Attard <jamatt@Jamess-MacBook-Pro.local>
2017-06-30 08:14:20 +0200

    Landing Page revamp

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes     = require("./routes/comments"),
     indexRoutes       = require("./routes/index");
 
 // Application configuration
-mongoose.connect("mongodb://localhost/yelp_camp_v11");
+mongoose.connect("mongodb://localhost/yelp_camp_v12");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
